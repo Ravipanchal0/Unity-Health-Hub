f16679b374e47cbb823e48dd7cb99634e25ae5af
Ravi <145011934+Ravipanchal0@users.noreply.github.com>
2023-11-10 22:13:15 +0530

    edit search component

--- src/components/js/Search.js
+++ src/components/js/Search.js
@@ -2,14 +2,10 @@ import React from "react";
 import "../css/Search.css";
 
 function Search(props) {
-  let inputValue = "";
-  function handleChange(event) {
-    inputValue = event.target.value;
-  }
   return (
     <>
       <div className="search-box">
-        <input id="search" type="search" className="search-input" name="searchBox" placeholder={props.search} onChange={handleChange} />
+        <input id="search" type="search" className="search-input" name="searchBox" placeholder={props.search} />
         <button id="search-btn" className="search-btn">
           search
         </button>
